edad554c12d21909db3c63936b25eeae63a20438
Decugis Allan <allan.decugis@edouardgand.fr>
2026-03-17 08:14:17 +0100

    correction exo2

---
+++ Movie.cs
@@ -0,0 +1,23 @@
+public class Movie
+{
+    private string title = string.Empty;
+    private int duree;
+
+    public string GetTitle()
+    {
+        return title;
+    }
+    public void SetTitle(string title)
+    {
+        this.title = title;
+    }
+
+    public int GetDuree()
+    {
+        return duree;
+    }
+    public void SetDuree(int duree)
+    {
+        this.duree = duree;
+    }
+} 
